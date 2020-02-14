11d36cefdac790f6656ade09b14ce1e968a42e98
megafeebas <55175492+megafeebas@users.noreply.github.com>
2020-02-13 19:13:16 -0500

    Castle

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -47,10 +47,12 @@ public class Cloud : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Restart();
         }
+        */
     }
 
     void Restart()
